e134c8706cd727e0ebb5d40f70d8b1fd5f2d9139
Charlotte Natasha <natashaonyango97@gmail.com>
2022-12-10 20:49:47 +0100

    added the stop function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let computerScore = 0;
 let playerScore = 0;
 let rounds = 0;
+let stop = false;
 
 const computerPlay = () => {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -14,10 +15,15 @@ const computerPlay = () => {
     }
 };
 
-const playerPlay = () => {
+const playerPlay = () => {    
     result = confirm("Let's play Rock, Paper, Scissors");
-    if (playRound) {
+    
+    if (result) {
         let playerPlay = prompt("Please enter: Rock, Paper or Scissors.");
+        if (playerPlay === null){
+            console.log("Error, please type a valid input");
+            return;
+        }
         playerPlay = playerPlay.toLowerCase();
         if (
             playerPlay === "rock" ||
@@ -29,9 +35,16 @@ const playerPlay = () => {
         console.log("Error, please type a valid input");
     }
     }
+    else{
+        alert("Sorry, Try again next time");
+        endGame();
+        stop = true;
+        
+    }
 };
 
 const playRound = (playerSelection, computerSelection) => {
+    
     if (playerSelection === computerSelection) {
         return `It's a tie! \n You played: ${playerSelection} Computer played: ${computerSelection}`;
     }
@@ -66,6 +79,7 @@ const playRound = (playerSelection, computerSelection) => {
 };
 
 const endGame = () => {
+    
     if (playerScore > computerScore ) {
         console.log(
             `Game Over! \nFinal score: \n Player: ${playerScore} -- Computer: ${computerScore}`
@@ -84,9 +98,8 @@ const endGame = () => {
 function game() {
     do {
         console.log(playRound(playerPlay(), computerPlay()));
-        // Counter here
         rounds += 1;
-    } while (rounds < 5);
+    } while (rounds < 5 && stop == false);
 
         endGame();
     }
